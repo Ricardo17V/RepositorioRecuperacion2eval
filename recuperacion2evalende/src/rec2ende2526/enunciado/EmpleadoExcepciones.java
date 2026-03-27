2b06733014533e3d32a5ae749d16c133f42761a7
ricardo <ricardo@DESKTOP-KLL37QO>
2026-03-27 09:45:32 +0100

    Implementacion de la clase test para testear los errores del método calcularSario(no terminado)

---
+++ recuperacion2evalende/src/rec2ende2526/enunciado/EmpleadoExcepciones.java
@@ -0,0 +1,15 @@
+package rec2ende2526.enunciado;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class EmpleadoExcepciones {
+@Test
+public void tenerPagaNegativa() {
+	
+}
+public void tenerMesMayor() {
+	
+}
+public void tenerMesMenor() {
+	
+}
+}
